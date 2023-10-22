4d132b362b6f4eeb3a93262bd9f0d56eebb4bd7b
qwerty142 <62697835+qwerty142@users.noreply.github.com>
2023-10-22 20:35:08 +0300

    Release 1

--- src/main/java/edu/hw2/Task3/ConnectionTypes/FaultyConnection.java
+++ src/main/java/edu/hw2/Task3/ConnectionTypes/FaultyConnection.java
@@ -1,7 +1,6 @@
 package edu.hw2.Task3.ConnectionTypes;
 
 import edu.hw2.Task3.Exception.ConnectionException;
-
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
