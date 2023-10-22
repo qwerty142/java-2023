bd810e457fbf70593fd11dce2ab5202eb8b56f95
qwerty142 <62697835+qwerty142@users.noreply.github.com>
2023-10-22 20:17:35 +0300

    ReWrite mvn config

--- src/test/java/edu/hw2/Task4Test.java
+++ src/test/java/edu/hw2/Task4Test.java
@@ -5,7 +5,6 @@ import edu.hw2.Task4.GetCallingData;
 import edu.hw2.Task4.Information;
 import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.assertThrows;
 public class Task4Test {
     @Test
     public void GetCallingInfo_shouldReturnNameAndClassWhereMethodWasCalled(){
