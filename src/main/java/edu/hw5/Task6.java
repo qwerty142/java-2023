281a1d0ae93af9213cecd600a95c93f518c5c4d5
qwerty142 <62697835+qwerty142@users.noreply.github.com>
2023-11-23 14:44:14 +0300

    Fix 2.1

--- src/main/java/edu/hw5/Task6.java
+++ src/main/java/edu/hw5/Task6.java
@@ -2,13 +2,15 @@ package edu.hw5;
 
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-import org.jetbrains.annotations.NotNull;
 
 public final class Task6 {
     private Task6() {}
 
     @SuppressWarnings("checkstyle:ParameterAssignment")
-    public static boolean checkOnSubString(@NotNull String sub, @NotNull String string) {
+    public static boolean checkOnSubString(String sub, String string) {
+        if (sub == null || string == null) {
+            throw new IllegalArgumentException();
+        }
         sub = "\\Q" + sub + "\\E";
         Pattern pattern = Pattern.compile(sub);
         Matcher matcher = pattern.matcher(string);
